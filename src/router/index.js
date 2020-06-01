7e37dd0c504a4162ea7030d04ca0cbc15df5cb1b
Mohammad Raska <iban.ninja@gmail.com>
2020-06-02 02:47:39 +0700

    add Not Found Page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/pages/PageHome'
 import ThreadShow from '@/components/pages/PageThreadShow'
+import NotFound from '@/components/pages/PageNotFound'
 
 Vue.use(Router)
 
@@ -9,7 +10,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
+      name: 'Home',
       component: Home
     },
     {
@@ -17,6 +18,11 @@ export default new Router({
       name: 'ThreadShow',
       component: ThreadShow,
       props: true
+    },
+    {
+      path: '*',
+      name: 'NotFound',
+      component: NotFound
     }
   ],
   mode: 'history'
