45a15f21ac37d89fcd331c1f1929fd90ddf0a751
Mohammad Raska <iban.ninja@gmail.com>
2020-06-04 06:42:29 +0700

    improve thread editor

--- src/store/index.js
+++ src/store/index.js
@@ -24,6 +24,7 @@ export default new Vuex.Store({
       commit('setPost', {post, postId})
       commit('appendPostToThread', {threadId: post.threadId, postId})
       commit('appendPostToUser', {userId: post.userId, postId})
+      return Promise.resolve(state.posts[postId])
     },
     createThread ({state, commit, dispatch}, {text, title, forumId}) {
       return new Promise((resolve, reject) => {
@@ -38,6 +39,9 @@ export default new Vuex.Store({
         commit('appendThreadToUser', {userId, threadId})
 
         dispatch('createPost', {text, threadId})
+          .then(post => {
+            commit('setThread', {threadId, thread: {...thread, firstPostId: post['.key']}})
+          })
         resolve(state.threads[threadId])
       })
     },
