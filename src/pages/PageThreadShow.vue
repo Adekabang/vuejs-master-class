c67624ace5f25a778817efc7b99359db8f72358c
Mohammad Raska <iban.ninja@gmail.com>
2020-06-04 03:56:20 +0700

    init vuex and change sourceData to vuex

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import sourceData from '@/data'
+
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
 
@@ -31,22 +31,22 @@ export default {
   },
   data () {
     return {
-      thread: sourceData.threads[this.id]
+      thread: this.$store.state.threads[this.id]
     }
   },
   computed: {
     posts () {
       const postsIds = Object.values(this.thread.posts)
-      return Object.values(sourceData.posts)
+      return Object.values(this.$store.state.posts)
         .filter(post => postsIds.includes(post['.key']))
     }
   },
   methods: {
     addPost ({post}) {
       const postId = post['.key']
-      this.$set(sourceData.posts, postId, post)
+      this.$set(this.$store.state.posts, postId, post)
       this.$set(this.thread.posts, postId, postId)
-      this.$set(sourceData.users[post.userId].posts, postId, postId)
+      this.$set(this.$store.state.users[post.userId].posts, postId, postId)
     }
   }
 }
