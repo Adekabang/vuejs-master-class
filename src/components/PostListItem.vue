2458ad5afb98bbec8651fcfeda0a0c46f6f5da10
Mohammad Raska <iban.ninja@gmail.com>
2020-06-04 02:19:02 +0700

    timestamp to date as a base component

--- src/components/PostListItem.vue
+++ src/components/PostListItem.vue
@@ -12,18 +12,19 @@
         {{post.text}}
         </div>
       </div>
-      <div class="post-date text-faded"
-        :title="post.publishedAt | humanFriendlyDate"
-      >
-        {{post.publishedAt | diffForHumans}}
+      <div class="post-date text-faded">
+        <AppDate :timestamp="post.publishedAt"/>
       </div>
     </div>
 </template>
 
 <script>
 import sourceData from '@/data'
-import moment from 'moment'
+import AppDate from '@/components/AppDate'
 export default {
+  components: {
+    AppDate
+  },
   props: {
     post: {
       required: true,
@@ -36,14 +37,6 @@ export default {
     },
     userPostCount () {
       return Object.keys(this.user.posts).length
-    }
-  },
-  filters: {
-    humanFriendlyDate (date) {
-      return moment.unix(date).format('MMMM Do YYYY, h:mm:ss a')
-    },
-    diffForHumans (date) {
-      return moment.unix(date).fromNow()
     }
   }
 }
