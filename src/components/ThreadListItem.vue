90f8be4f0a15e0300cf5a60df4e92539beddfbd3
Mohammad Raska <iban.ninja@gmail.com>
2020-06-04 02:27:08 +0700

    AppDate to global  base component

--- src/components/ThreadListItem.vue
+++ src/components/ThreadListItem.vue
@@ -30,11 +30,7 @@
 
 <script>
 import sourceData from '@/data'
-import AppDate from '@/components/AppDate'
 export default {
-  components: {
-    AppDate
-  },
   props: {
     thread: {
       required: true,
