e0f6c284f431a6ed0e5f913cc421a1a6a001c43a
Mohammad Raska <iban.ninja@gmail.com>
2020-06-05 02:16:41 +0700

    user env variable in webpack template

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,12 @@
 'use strict'
 module.exports = {
-  NODE_ENV: '"production"'
+  FIREBASE_NODE_ENV: '"production"',
+  FIREBASE_API_KEY: '"AIzaSyADU1sD6hBT2Li2ZLZIRzNksfS7qD-0kzk"',
+  FIREBASE_AUTH_DOMAIN: '"vue-school-forum-d4578.firebaseapp.com"',
+  FIREBASE_DATABASE_URL: '"https://vue-school-forum-d4578.firebaseio.com"',
+  FIREBASE_PROJECT_ID: '"vue-school-forum-d4578"',
+  FIREBASE_STORAGE_BUCKET: '"vue-school-forum-d4578.appspot.com"',
+  FIREBASE_MESSAGING_SENDER_ID: '"184085520243"',
+  FIREBASE_APP_ID: '"1:184085520243:web:5b30c69b6e2b9bc1fcaf06"',
+  FIREBASE_MEASUREMENT_ID: '"G-S506VDRK3D"'
 }
--- src/main.js
+++ src/main.js
@@ -13,14 +13,14 @@ Vue.config.productionTip = false
 
 // Firebase Config
 const firebaseConfig = {
-  apiKey: 'AIzaSyADU1sD6hBT2Li2ZLZIRzNksfS7qD-0kzk',
-  authDomain: 'vue-school-forum-d4578.firebaseapp.com',
-  databaseURL: 'https://vue-school-forum-d4578.firebaseio.com',
-  projectId: 'vue-school-forum-d4578',
-  storageBucket: 'vue-school-forum-d4578.appspot.com',
-  messagingSenderId: '184085520243',
-  appId: '1:184085520243:web:5b30c69b6e2b9bc1fcaf06',
-  measurementId: 'G-S506VDRK3D'
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
